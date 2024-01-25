4c9b3f054c3c81d3e56cb7c4dab9cc5b18b53b41
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-01-25 10:46:26 -0300

    função que retorna o filme de acordo com o id finalizada

--- controller/funcoes.js
+++ controller/funcoes.js
@@ -19,3 +19,31 @@ const getFilmes = function(){
    }
 }
 
+const getFilme = function(id){
+    const jsonFilme = {}
+    let status = false
+
+    filmes.filmes.filmes.forEach(filmesA => {
+        if(id == filmesA.id){
+            jsonFilme.id = filmesA.id
+            jsonFilme.nome = filmesA.nome
+            jsonFilme.sinopse = filmesA.sinopse
+            jsonFilme.duracao = filmesA.duracao
+            jsonFilme.data_lancamento = filmesA.data_lancamento
+            jsonFilme.data_relancamento = filmesA.data_relancamento
+            jsonFilme.foto_capa = filmesA.foto_capa
+            jsonFilme.valor_unitario = filmesA.valor_unitario
+
+            status = true
+        }        
+    });
+
+    if(filmes){
+        return jsonFilme
+    }else{
+        return false
+    }
+}
+
+console.log(getFilme(3))
+
