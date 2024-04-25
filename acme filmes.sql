aa0a6c867a764eedb4c4ab8224212b7fdf7b8907
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-04-25 17:04:38 -0300

    atualização arquivo sql

--- acme filmes.sql
+++ acme filmes.sql
@@ -6,6 +6,12 @@ create table tbl_favorito(
 id integer primary key not null auto_increment,
 favorito boolean not null
 ); 
+insert into tbl_favorito(favorito) values
+(true),
+(false);
+
+select * from tbl_favorito;
+
 
 create table tbl_classificacao(
 id integer primary key not null auto_increment,
@@ -77,18 +83,44 @@ data_lancamento date not null,
 data_relancamento date,
 foto_capa varchar(100) not null,
 valor_unitario float not null,
+
 id_favorito integer not null,
 constraint FK_FAVORITO_FILME
 foreign key(id_favorito) references tbl_favorito(id),
+
 id_classificacao integer not null,
 constraint 	FK_CLASSIFICACAO_FILME
 foreign key(id_classificacao) references tbl_classificacao(id)
 );
+insert into tbl_filme( 
+        titulo,
+        sinopse,
+        duracao,
+        data_lancamento,
+        data_relancamento,
+        foto_capa,
+        valor_unitario,
+        id_favorito,
+        id_classificacao
+)values(
+        'Pantera Negra',
+        'Em Pantera Negra, após a morte do rei T Chaka (John Kani), o príncipe T Challa (Chadwick Boseman) retorna a Wakanda para a cerimônia de coroação. Nela são reunidas as cinco tribos que compõem o reino, sendo que uma delas, os Jabari, não apoia o atual governo. T Challa logo recebe o apoio de Okoye (Danai Gurira), a chefe da guarda de Wakanda, da irmã Shuri (Letitia Wright), que coordena a área tecnológica do reino, e também de Nakia (Lupita Nyong o), a grande paixão do atual Pantera Negra, que não quer se tornar rainha. Juntos, eles estão à procura de Ulysses Klaue (Andy Serkis), que roubou de Wakanda um punhado de vibranium, alguns anos atrás.',
+        '02:15',
+        '2018-02-15',
+         null,
+        'https://br.web.img3.acsta.net/c_310_420/pictures/17/12/07/16/09/2291532.jpg',
+        '34.99',
+        '1',
+        '8'
+);
+
 
 create table tbl_sexoA(
 id integer not null primary key auto_increment,
 sexo varchar(15)
 );
+insert into tbl_sexoA(sexo) values ('homem'), ('mulher');
+select * from tbl_sexoA;
 
 create table tbl_nacionalidadeA(
 id integer not null primary key auto_increment,
@@ -114,6 +146,22 @@ id_sexoA integer not null,
 constraint FK_SEXOA_ATOR
 foreign key (id_sexoA) references tbl_sexoA(id)
 );
+insert into tbl_ator(nome, foto_ator, biografia, id_sexoA) values 
+(
+'Adam Sandlerr',
+'https://br.web.img3.acsta.net/c_310_420/pictures/17/06/20/16/57/103535.jpg',
+'Adam Sandler é um ator, comediante e dublador norte-americano, mais conhecido por ser o pai da comédia pastelão. Sandler iniciou a carreira de humorista bem cedo, pois, aos 17 anos, começou a fazer apresentações de stand-up no Boston Comedy Club. Após se formar no curso de Belas Artes na New York University, o jovem mudou-se para Los Angeles e deu sequência na área do humor, onde acabou sendo descoberto pelo comediante Dennis Miller, que o recomendou ao produtor de Saturday Night Live. Sendo assim, em 1990, Sandler foi contratado pelo programa da NBC como roteirista, passando a fazer parte do elenco regular, no ano seguinte.Quatro anos depois, Adam Sandler deixou o SNL para começar a carreira de ator. Seu primeiro papel de destaque foi em Billy Madison, um Herdeiro Bobalhão (1995), onde interpretou o protagonista.',
+1
+);
+update tbl_ator
+                      set
+                      nome = 'testeP',
+                      foto_ator = 'https://br.web.img3.acsta.net/c_310_420/pictures/17/06/20/16/57/103535.jpg',
+                      biografia = 'Adam Sandler é um ator, comediante e dublador norte-americano, mais conhecido por ser o pai da comédia pastelão. Sandler iniciou a carreira de humorista bem cedo, pois, aos 17 anos, começou a fazer 
+apresentações de stand-up no Boston Comedy Club. Após se formar no curso de Belas Artes na New York University, o jovem mudou-se para Los Angeles e deu sequência na área do humor, onde acabou sendo descoberto pelo comediante Dennis Miller, que o recomendou ao produtor d...',
+                      id_sexoA = 2
+                      
+                      where id = 12;
 
 create table tbl_ator_filme(
 id integer not null primary key auto_increment,
@@ -166,19 +214,8 @@ constraint FK_ATOR_DIRETORATOR
 foreign key(id_ator) references tbl_ator(id)
 );
 
-
-INSERT INTO tbl_filme (nome, sinopse, duracao, data_lancamento, foto_capa, valor_unitario) VALUES 
-(
-'Vingadores: Ultimato', 
-'Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, 
-os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. Com Tony Stark (Robert Downey Jr.) 
-vagando perdido no espaço sem água nem comida, o Capitão América/Steve Rogers (Chris Evans) e a Viúva Negra/Natasha Romanov 
-(Scarlett Johansson) precisam liderar a resistência contra o titã louco.',
-'03:01',
-'2019-04-25',
-'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
-29.99
-),
+select * from tbl_filme;
+INSERT INTO tbl_filme (titulo, sinopse, duracao, data_lancamento, foto_capa, valor_unitario, id_favorito, id_classificacao) VALUES 
 (
 'Interestelar',
 'Após ver a Terra consumindo boa parte de suas reservas naturais, um grupo de astronautas recebe a missão de verificar
@@ -189,7 +226,22 @@ vagando perdido no espaço sem água nem comida, o Capitão América/Steve Roger
  '02:49',
  '2014-11-06',
  'https://br.web.img3.acsta.net/c_310_420/pictures/14/10/31/20/39/476171.jpg',
- 29.99
+ 29.99,
+ 2,
+ 8
+),
+(
+'Vingadores: Ultimato', 
+'Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, 
+os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. Com Tony Stark (Robert Downey Jr.) 
+vagando perdido no espaço sem água nem comida, o Capitão América/Steve Rogers (Chris Evans) e a Viúva Negra/Natasha Romanov 
+(Scarlett Johansson) precisam liderar a resistência contra o titã louco.',
+'03:01',
+'2019-04-25',
+'https://br.web.img2.acsta.net/c_310_420/pictures/19/04/26/17/30/2428965.jpg',
+29.99,
+'1',
+8
 );
 
 show tables;
