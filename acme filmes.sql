9f6a491993ba958a654252e74ee702a4430f7f71
vitorkolle <vitorpaeskolle@gmail.com>
2024-05-02 02:45:55 -0300

    arquivo sql

--- acme filmes.sql
+++ acme filmes.sql
@@ -106,8 +106,8 @@ insert into tbl_filme(
          null,
         'https://br.web.img3.acsta.net/c_310_420/pictures/17/12/07/16/09/2291532.jpg',
         '34.99',
-        1,
-        1
+        '1',
+        '8'
 );
 
 ##SEXO ATOR
@@ -137,6 +137,8 @@ id_ator integer not null,
 constraint FK_ATOR_NACIONALIDADEAATOR
 foreign key (id_ator) references tbl_ator(id)
 );
+insert into tbl_nacionalidadeAator(id_nacionalidadeA, id_ator)values
+(3, 1);
 
 ##ATOR
 create table tbl_ator(
@@ -148,6 +150,12 @@ id_sexoA integer not null,
 constraint FK_SEXOA_ATOR
 foreign key (id_sexoA) references tbl_sexoA(id)
 );
+alter table tbl_ator
+add column data_nascimento date not null;
+
+delete from tbl_ator where id = 5;
+
+
 
 insert into tbl_ator(nome, foto_ator, biografia, id_sexoA) values 
 (
@@ -156,15 +164,7 @@ insert into tbl_ator(nome, foto_ator, biografia, id_sexoA) values
 'Adam Sandler é um ator, comediante e dublador norte-americano, mais conhecido por ser o pai da comédia pastelão. Sandler iniciou a carreira de humorista bem cedo, pois, aos 17 anos, começou a fazer apresentações de stand-up no Boston Comedy Club. Após se formar no curso de Belas Artes na New York University, o jovem mudou-se para Los Angeles e deu sequência na área do humor, onde acabou sendo descoberto pelo comediante Dennis Miller, que o recomendou ao produtor de Saturday Night Live. Sendo assim, em 1990, Sandler foi contratado pelo programa da NBC como roteirista, passando a fazer parte do elenco regular, no ano seguinte.Quatro anos depois, Adam Sandler deixou o SNL para começar a carreira de ator. Seu primeiro papel de destaque foi em Billy Madison, um Herdeiro Bobalhão (1995), onde interpretou o protagonista.',
 1
 );
-update tbl_ator
-                      set
-                      nome = 'testeP',
-                      foto_ator = 'https://br.web.img3.acsta.net/c_310_420/pictures/17/06/20/16/57/103535.jpg',
-                      biografia = 'Adam Sandler é um ator, comediante e dublador norte-americano, mais conhecido por ser o pai da comédia pastelão. Sandler iniciou a carreira de humorista bem cedo, pois, aos 17 anos, começou a fazer 
-apresentações de stand-up no Boston Comedy Club. Após se formar no curso de Belas Artes na New York University, o jovem mudou-se para Los Angeles e deu sequência na área do humor, onde acabou sendo descoberto pelo comediante Dennis Miller, que o recomendou ao produtor d...',
-                      id_sexoA = 2
-                      
-                      where id = 12;
+
 
 ##ATOR - FILME
 create table tbl_ator_filme(
@@ -176,6 +176,24 @@ id_filme integer not null,
 constraint FK_FILME_ATORFILME
 foreign key (id_filme) references tbl_filme(id)
 );
+insert into tbl_ator_filme(id_ator, id_filme)values
+(1, 3);
+
+select * from tbl_ator_filme;
+
+insert into tbl_ator_filme
+
+        (
+            id_ator,
+            id_filme
+          )
+          values(
+            1,
+            24
+          )
+;
+delete from tbl_diretor where id = 1;
+
 
 ##SEXO(DIRETOR)
 create table tbl_sexoD(
@@ -196,10 +214,19 @@ insert into tbl_nacionalidadeD(pais)values
 create table tbl_nacionalidadeD_diretor(
 id integer not null primary key auto_increment,
 id_nacionalidadeD integer not null,
+constraint FK_NACIONALIDADED_NACIONALIDADEDDIRETOR
 foreign key(id_nacionalidadeD) references tbl_nacionalidadeD(id),
 id_diretor integer not null,
+constraint FK_DIRETOR_NACIONALIDADEDDIRETOR
 foreign key(id_diretor) references tbl_diretor(id)
 );
+insert into tbl_nacionalidadeD_diretor(id_nacionalidadeD, id_diretor)values
+(
+1,
+1
+);
+
+select * from tbl_nacionalidadeD_diretor;
 
 ##DIRETOR
 create table tbl_diretor(
@@ -212,9 +239,71 @@ id_sexoD integer not null,
 constraint FK_SEXOD_DIRETOR
 foreign key(id_sexoD) references tbl_sexoD(id)
 );
+insert into tbl_diretor(nome, foto_diretor, biografia, data_nascimento, id_sexoD)values
+(
+'Diretor Teste',
+'ogudfbsvouebroudnb',
+'biografia',
+'2020-12-13',
+1
+);
 
-alter table tbl_diretor
-drop column id_nacionalidade;
+##DIRETOR - FILME
+create table tbl_diretor_filme(
+id integer not null primary key auto_increment,
+id_diretor integer not null,
+constraint FK_DIRETOR_DIRETORFILME
+foreign key (id_diretor) references tbl_diretor(id),
+id_filme integer not null,
+constraint FK_FILME_DIRETORFILME
+foreign key (id_filme) references tbl_filme(id)
+);
+insert into tbl_diretor_filme(id_diretor, id_filme)values
+(1, 3);
+        
+select * from tbl_diretor;
+select * from tbl_diretor_filme where id_diretor = 8;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+delete from tbl_diretor where id = 7;
+
+SET FOREIGN_KEY_CHECKS=1;
+
+delete from tbl_filme where id = 3;
+
+alter table tbl_ator
+drop foreign key FK_SEXOA_ATOR;
+
+alter table tbl_ator_filme
+drop foreign key FK_ATOR_ATORFILME;
+
+delete from tbl_ator where id = 3;
+
+alter table tbl_ator
+add constraint FK_SEXOA_ATOR
+    foreign key (id_sexoA)
+    references tbl_sexoA(id);
+    
+alter table tbl_ator_filme
+add constraint FK_ATOR_ATORFILME
+    foreign key (id_ator)
+    references tbl_ator(id);
+    
+		alter table tbl_ator
+        drop foreign key FK_SEXOA_ATOR;
+
+		SET FOREIGN_KEY_CHECKS=0;
+
+        delete from tbl_ator where id = 1;
+        
+        SET FOREIGN_KEY_CHECKS=1;
+
+        alter table tbl_ator
+        add constraint FK_SEXOA_ATOR
+        foreign key (id_sexoA)
+        references tbl_sexoA(id);
+
 
 ##DIRETOR - ATOR
 create table tbl_diretor_ator(
@@ -226,7 +315,21 @@ id_ator integer not null,
 constraint FK_ATOR_DIRETORATOR
 foreign key(id_ator) references tbl_ator(id)
 );
+select * from tbl_diretor_filme where id_diretor = 3;
 
+insert into tbl_diretor_filme
+
+        (
+            id_diretor,
+            id_filme
+        )
+          values(
+            9,
+            3
+          );
+          
 show tables;
+
+select * from tbl_filme;
 
 select cast(last_insert_id() as decimal) as id from tbl_filme limit 1;
